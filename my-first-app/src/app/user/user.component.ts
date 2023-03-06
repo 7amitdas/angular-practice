bcee0aa8797285782c786d2a525eeaf9b5432859
Amit Das <7amitdas@gmail.com>
2023-03-06 22:07:09 +0800

    Added Display Button and toggle a paragraph on click. Logging All the button click in array and from fifth item change color of Button Log.

--- my-first-app/src/app/user/user.component.ts
+++ my-first-app/src/app/user/user.component.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  styles: [
+    `
+      .colWhite {
+        color: white;
+      }
+    `,
+  ],
 })
 export class UserComponent {
   username: string = '';
   //isAddUserButtonEnabled = false;
+  displaySecretPass: boolean = false;
+  buttonLog: number[] = [];
+  // changeLogBackgroundColor: boolean = false;
+  // changeLogTextColor: boolean = false;
 
   constructor() {}
 
@@ -22,4 +33,21 @@ export class UserComponent {
   //     this.username = '';
   //     //this.isAddUserButtonEnabled = false;
   //   }
+
+  onClickDisplayDetails() {
+    this.displaySecretPass = !this.displaySecretPass;
+    if (this.displaySecretPass) {
+      this.buttonLog.push(1);
+    } else {
+      this.buttonLog.push(0);
+    }
+    // if (this.buttonLog.length >= 5) {
+    //   this.changeLogBackgroundColor = true;
+    //   this.changeLogTextColor = true;
+    // }
+  }
+
+  getLogBackGroundColor(indexOfelement): string {
+    return indexOfelement >= 4 ? 'blue':'white';
+  }
 }
