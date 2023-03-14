1e54279f90329908e0761710c2827df9fb5a696e
Amit Das <7amitdas@gmail.com>
2023-03-14 21:05:16 +0800

    Relative Button Disabled with Start, Pause and Stop Button Clicked

--- my-first-app/src/app/game-control/game-control.component.ts
+++ my-first-app/src/app/game-control/game-control.component.ts
@@ -9,28 +9,39 @@ export class GameControlComponent implements OnInit {
   counter: number = 0;
   @Output('startGame') intervalFired = new EventEmitter<number>();
   @Output('stopGame') stopGame = new EventEmitter<number>();
-  interval:any;
+  interval: any;
+
+  isStartButtonDisabled: boolean = false;
+  isPauseButtonDisabled: boolean = true;
+  isStopButtonDisabled: boolean = true;
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onClickStart() {
     this.interval = setInterval(() => {
       this.intervalFired.emit(this.counter + 1);
       this.counter++;
     }, 1000);
+    this.isStartButtonDisabled = true;
+    this.isPauseButtonDisabled = false;
+    this.isStopButtonDisabled = false;
   }
 
   onClickPause() {
     clearInterval(this.interval);
+    this.isStartButtonDisabled = false;
+    this.isPauseButtonDisabled = true;
+    this.isStopButtonDisabled = false;
   }
 
   onClickStop() {
     this.counter = 0;
     clearInterval(this.interval);
     this.stopGame.emit(this.counter);
+    this.isStartButtonDisabled = false;
+    this.isPauseButtonDisabled = true;
+    this.isStopButtonDisabled = true;
   }
 }
