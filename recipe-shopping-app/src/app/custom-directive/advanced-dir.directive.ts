7f6fb5afe35ed36d428c1f314695e066260fd81a
Amit Das <7amitdas@gmail.com>
2023-03-18 22:51:04 +0800

    Using Host Listener to Listen to DOM Events

--- recipe-shopping-app/src/app/custom-directive/advanced-dir.directive.ts
+++ recipe-shopping-app/src/app/custom-directive/advanced-dir.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancedDir]',
@@ -7,6 +7,26 @@ export class AdvancedDirDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    // By using renderer instead of directly accessing dom element is a better approach as Angular might not run always in Browser.
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'blue'
+    );
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'yellow'
+    );
+  }
+
+  // Using Mouse Leave instead of Mouse Out Event
+  @HostListener('mouseleave')
+  onMouseExit(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
