0f23551f43e73b5bd052887d50940e33b413536b
Amit Das <7amitdas@gmail.com>
2023-04-16 21:20:36 +0800

    Custom Observable Creation

--- my-first-app/src/app/observ-home/observ-home.component.ts
+++ my-first-app/src/app/observ-home/observ-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observ-home',
   templateUrl: './observ-home.component.html',
-  styleUrls: ['./observ-home.component.css']
+  styleUrls: ['./observ-home.component.css'],
 })
-export class ObservHomeComponent {
+export class ObservHomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
+  ngOnInit(): void {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log('Count:::::' + count);
+    // });
+    const customIntervalObserable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+    this.firstObsSubscription = customIntervalObserable.subscribe((data) => {
+      console.log('Count:::::' + data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
