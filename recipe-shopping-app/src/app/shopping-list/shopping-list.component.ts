d1132d87495d3624b812587ff9a46606f3568e93
Amit Das <7amitdas@gmail.com>
2023-04-18 21:34:25 +0800

    Added Subcription, Subject

--- recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../model/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,19 +8,24 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService : ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
       (inds: Ingredient[]) => {
         this.ingredients = inds;
       }
     );
   }
+
+ngOnDestroy(): void {
+  this.subscription.unsubscribe();
+}
 
   onAdd(event: Ingredient) {
     //  this.ingredients.push(event);
